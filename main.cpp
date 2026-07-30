cf68fc0a5cc5fad1f64a6b8c833456c99a40f249
vagif <vagifaghayev270298@gmail.com>
2026-07-30 21:03:30 +0900

    initial triangles setup

--- main.cpp
+++ main.cpp
@@ -1,56 +1,65 @@
-#include "tgaimage.h"
-#include <algorithm>
 #include <cmath>
-#include <cstdlib>
-#include <ctime>
-#include <cstdlib>le
+#include <tuple>
+#include "geometry.h"
+#include "model.h"
+#include "tgaimage.h"
 
-// compile time constants - do not depent on runtime input or file I/O, dynamic memory allocation, etc.
+constexpr int width = 128;
+constexpr int height = 128;
+
 constexpr TGAColor white = {255, 255, 255, 255};
 constexpr TGAColor green = {0, 255, 0, 255};
 constexpr TGAColor red = {0, 0, 255, 255};
 constexpr TGAColor blue = {255, 128, 64, 255};
 constexpr TGAColor yellow = {0, 200, 255, 255};
 
-// draw lines/triangle connecting dots
-void line(int aX, int aY, int bX, int bY, TGAImage &buffer, TGAColor color)
+void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color)
 {
-    float steps = std::max(std::abs(bX - aX), std::abs(bY - aY));
-    for (int i = 0; i <= steps; ++i)
+    bool steep = std::abs(ax - bx) < std::abs(ay - by);
+    if (steep)
     {
-        float stepProgress = i / steps;
-        int x = std::round(aX + (bX - aX) * stepProgress);
-        int y = std::round(aY + (bY - aY) * stepProgress);
-
-        buffer.set(x, y, color);
+        std::swap(ax, ay);
+        std::swap(bx, by);
+    }
+    if (ax > bx)
+    {
+        std::swap(ax, bx);
+        std::swap(ay, by);
+    }
+    int y = ay;
+    int ierror = 0;
+    for (int x = ax; x <= bx; x++)
+    {
+        if (steep)
+        {
+            framebuffer.set(y, x, color);
+        }
+        else
+        {
+            framebuffer.set(x, y, color);
+        }
+        ierror += 2 * std::abs(by - ay);
+        if (ierror > bx - ax)
+        {
+            y += by > ay ? 1 : -1;
+            ierror -= 2 * (bx - ax);
+        }
     }
 }
 
-// argument count and vector as params
+void triangle(int ax, int ay, int bx, int by, int cx, int cy, TGAImage &framebuffer, TGAColor color)
+{
+    line(ax, ay, bx, by, framebuffer, color);
+    line(bx, by, cx, cy, framebuffer, color);
+    line(cx, cy, ax, ay, framebuffer, color);
+}
+
 int main(int argc, char **argv)
 {
-    constexpr int width = 64;
-    constexpr int height = 64;
     TGAImage framebuffer(width, height, TGAImage::RGB);
-
-    // int aX = 7, aY = 3;
-    // int bX = 12, bY = 34;
-    // int cX = 60, cY = 53;
-
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
-
-    for (int i = 0; i < (1 << 24); i++)
-    {
-        const int ax = std::rand() % width;
-        const int ay = std::rand() % height;
-        const int bx = std::rand() % width;
-        const int by = std::rand() % height;
-        line(ax, ay, bx, by, framebuffer, {static_cast<std::uint8_t>(std::rand() % 256), static_cast<std::uint8_t>(std::rand() % 256), static_cast<std::uint8_t>(std::rand() % 256), static_cast<std::uint8_t>(std::rand() % 256)});
-    }
-
-    // framebuffer.set(aX, aY, white);
-    // framebuffer.set(bX, bY, green);
-    // framebuffer.set(cX, cY, red);
-
-    framebuffer.write_tga_file("triangle.tga");
+    triangle(7, 45, 35, 100, 45, 60, framebuffer, red);
+    triangle(120, 35, 90, 5, 45, 110, framebuffer, white);
+    triangle(115, 83, 80, 90, 85, 120, framebuffer, green);
+    framebuffer.write_tga_file("framebuffer.tga");
+    return 0;
 }
